69966435c5bd14c48762f2d4815b1eab32acfe4e
mathew <meta@pobox.com>
2017-08-08 20:05:05 -0500

    First cut of converting my Ruby scripts

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # flactools
-Go-based tools for FLAC music collection maintenance
+
+_Go-based tools for FLAC music collection maintenance_
+
+This repository contains tools I use to manage FLAC music files on my
+Synology NAS.
+
+At the moment, the two utilities are `flaccat` and `flacdiff`. Used together,
+they offer a way to check your music library for additions, deletions, and file
+moves.
+
+    % flaccat /volume1/FLAC > ~/flaclist.old
+    [ some weeks later ]
+    % flaccat /volume1/FLAC > ~/flaclist.new
+    % flaccat ~/flaclist.old ~/flaclist.new
+
+## Installation
+
+Because the utilities are all written in pure Go, and use only pure Go
+libraries, it should be easy to cross-compile them for any Synology box,
+whether it's Intel-based or ARM-based.
+
+For example, to build a 64-bit ARM binary of flaccat:
+
+    cd flaccat
+    GOOS=linux GOARCH=arm64 go build
+
+## License
+
+GPL v3.
+
+## Contact
+
+mathew  
+<meta@pobox.com>
+
---
+++ flaccat/flaccat.go
@@ -0,0 +1,56 @@
+// flaccat
+//
+// Usage:
+//   flaccat <path>
+//
+// Scans all directories under the specified path for FLAC files, and outputs
+// a catalog file to standard output consisting of the MD5 checksum for each
+// FLAC file, followed by the path to the file.
+//
+// By comparing successive versions of the output, you can determine which
+// flac files have been added, removed, or moved. The flacdiff utility will
+// perform that function.
+//
+package main
+
+import (
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/mewkiz/flac"
+)
+
+func checksum(fspc string) (string, error) {
+	stream, err := flac.ParseFile(fspc)
+	if err != nil {
+		return "", err
+	}
+	defer stream.Close()
+	info := stream.Info
+	md5 := hex.EncodeToString(info.MD5sum[:])
+	return md5, nil
+}
+
+func examine(path string, info os.FileInfo, err error) error {
+	lcpath := strings.ToLower(path)
+	if strings.HasSuffix(lcpath, ".flac") {
+		md5, ferr := checksum(path)
+		if ferr != nil {
+			fmt.Fprintf(os.Stderr, "error reading %s: %s", path, ferr)
+		} else {
+			fmt.Printf("%s %s\n", md5, path)
+		}
+	}
+	return err
+}
+
+func main() {
+	flag.Parse()
+	for _, fname := range flag.Args() {
+		filepath.Walk(fname, examine)
+	}
+}
